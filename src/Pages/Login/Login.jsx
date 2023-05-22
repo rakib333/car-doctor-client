c57ae3796ac872a3e12cbb6df06d2d5420769cfd
rakib333 <smrakib333@gmail.com>
2023-05-23 00:55:11 +0600

    added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,29 @@
 import { Link } from 'react-router-dom';
 import loginImg from '../../assets/images/login/login.svg';
+import { useContext } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
+
 const Login = () => {
+
+    const { signIn } = useContext(AuthContext);
+
     const handleLogin = event => {
         event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password)
+
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.log(error));
+
     }
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,9 +1,28 @@
 import { Link } from 'react-router-dom';
 import loginImg from '../../assets/images/login/login.svg';
+import { useContext } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
 
 const SignUp = () => {
+    const {createUser} = useContext(AuthContext);
+
     const handleSignUp = event => {
         event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(name, email, password)
+
+        createUser(name, email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user)
+        })
+        .catch((err)=>console.log(err))
+
+
     }
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -19,25 +38,25 @@ const SignUp = () => {
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" placeholder="Name" className="input input-bordered" />
+                                <input type="text" placeholder="Name" name='name' className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="text" placeholder="email" name='email' className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input type="text" placeholder="password" name='password' className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <input type="submit" className="btn btn-primary" value="Login" />
+                                <input type="submit" className="btn btn-primary" value="Sign Up" />
                             </div>
                         </form>
                         <h3>Already have an account? <Link to='/login' className='text-red-800'>Login</Link></h3>
